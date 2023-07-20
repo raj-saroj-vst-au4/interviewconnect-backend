de77e37b3fa2826d6496c8c44abde4f241275696
raj-saroj-vst-au4 <raj-saroj-vst-au4>
2023-07-20 13:09:11 +0530

    backend pushed

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    method: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("disconnected");
+  });
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+  socket.on("ansCall", (data) => {
+    io.to(data.to).emit("callAccepted"), data.signal;
+  });
+});
+
+server.listen(process.env.PORT || 8000, () => {
+  console.log("Server running");
+});
